1814f9d70a9587ac50752712272beb7b3e02a8b0
Devendra Choudhary <dk_chy1981@yahoo.co.in>
2024-11-12 17:39:51 +0530

    Fourth Commit

--- CleanArchitectureDemo/TokenUtils.cs
+++ CleanArchitectureDemo/TokenUtils.cs
@@ -2,7 +2,6 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Security.Cryptography;
 using System.Text;
-using Microsoft.AspNetCore.DataProtection;
 using Microsoft.IdentityModel.Tokens;
 
 namespace CleanArchitectureDemo.WebAPI.Utility
